a2cd5fde0dfa5add122a9790da0319828f1a6e3d
SashrikaPrasad <77523087+SashrikaPrasad@users.noreply.github.com>
2021-03-12 18:06:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+lipY= 0
+
+function preload() {
+clown_nose= loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup() {
+canvas = createCanvas(300 , 300);
+canvas.center();
+video= createCapture(VIDEO);
+video.size(300 , 300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotposes);
+
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
